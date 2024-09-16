389f9fe1aa400f3a034b395a8107e60163a98c43
Jan David <jdno@jdno.dev>
2024-09-16 06:17:46 -0400

    Remove dedicated crate for shared types (#15)
    After further investigating, the crate for shared types does not seem
    necessary. The derive macros do not need access to the types, only once
    the macro is expended those need to be visible. With that in mind, it
    seems better to keep everything user-facing in the `doco` crate.
    
    This reverts commit 011c5c79ca1b085132203ca3add9a0b40bec1ebe.

--- crates/doco/src/lib.rs
+++ crates/doco/src/lib.rs
@@ -3,14 +3,45 @@
 //! `doco` is a framework and runner for end-to-tests of web applications.
 
 pub use anyhow::{Context, Error, Result};
-pub use doco_types::*;
 pub use fantoccini::Locator;
+use getset::Getters;
+use typed_builder::TypedBuilder;
 
 pub use crate::client::Client;
+pub use crate::server::Server;
 pub use crate::test_runner::*;
 
 mod client;
+mod server;
 mod test_runner;
 
 #[cfg(test)]
 mod test_utils;
+
+#[derive(Clone, Eq, PartialEq, Ord, PartialOrd, Hash, Debug, Default, Getters, TypedBuilder)]
+pub struct Doco {
+    #[getset(get = "pub")]
+    server: Server,
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::test_utils::*;
+
+    use super::*;
+
+    #[test]
+    fn trait_send() {
+        assert_send::<Doco>();
+    }
+
+    #[test]
+    fn trait_sync() {
+        assert_sync::<Doco>();
+    }
+
+    #[test]
+    fn trait_unpin() {
+        assert_unpin::<Doco>();
+    }
+}
