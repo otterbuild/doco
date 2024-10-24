11fbe8df268ca1600b20e4049111509d5181d875
Jan David <jdno@jdno.dev>
2024-10-24 16:41:56 +0200

    Add optional environment variables to the server
    The server can now be configured with optional environment variables.

--- crates/doco/src/server.rs
+++ crates/doco/src/server.rs
@@ -4,6 +4,8 @@ use getset::{CopyGetters, Getters};
 use testcontainers::core::WaitFor;
 use typed_builder::TypedBuilder;
 
+use crate::environment::Variable;
+
 /// Server for the web application that is being tested
 ///
 /// The `Server` struct configures the server that is being tested. Doco runs the server as a Docker
@@ -24,6 +26,15 @@ pub struct Server {
     #[getset(get_copy = "pub")]
     port: u16,
 
+    /// Environment variables to set in the service's container
+    #[builder(via_mutators(init = Vec::new()), mutators(
+        pub fn env(mut self, name: impl Into<String>, value: impl Into<String>) {
+            self.envs.push(Variable::new(name, value));
+        }
+    ))]
+    #[getset(get = "pub")]
+    envs: Vec<Variable>,
+
     /// An optional condition to wait until the server has properly started
     #[builder(default, setter(into))]
     #[getset(get = "pub")]
@@ -35,6 +46,19 @@ mod tests {
     use crate::test_utils::*;
 
     use super::*;
+
+    #[test]
+    fn env_collects_variables() {
+        let server = Server::builder()
+            .image("doco")
+            .tag("latest")
+            .port(8080)
+            .env("LOG_LEVEL", "debug")
+            .env("PORT", "8080")
+            .build();
+
+        assert_eq!(2, server.envs.len());
+    }
 
     #[test]
     fn trait_send() {
